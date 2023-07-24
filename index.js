a010998a03dc38c6e288283a2e58076ba2b1c787
Hargent <yemisi3385@gmail.com>
2023-07-25 00:16:30 +0100

    fix(mobile navigation page): Fixed the error in closing of the modal.
    chore!(VR section of the main body of the page): Making the VR image and text
    align and respond to width change.
    
    BREAKING-CHANGE!: Removed all height related css properties.

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const ParentEl = document.querySelector(".header");
 const dateEl = document.querySelector(".footer__year");
 
-let state = false;
-
 const handleOpenMobileNav = e => {
 	const navBtn = e.target.closest(".header__mobile__nav-icon");
-	console.log(navBtn);
 	if (!navBtn) return;
-	const mobileNav = ParentEl.querySelector(".header__mobile__nav");
-	mobileNav.classList.toggle("show");
+	console.log(navBtn);
+	const mobileNav = ParentEl.querySelector(".mobile__menu");
+	mobileNav.classList.toggle("toggle-display");
 };
 
 ParentEl.addEventListener("click", handleOpenMobileNav);
@@ -16,4 +14,3 @@ ParentEl.addEventListener("click", handleOpenMobileNav);
 // year generation
 const date = new Date();
 const year = date.getFullYear();
-dateEl.innerHTML = year;
